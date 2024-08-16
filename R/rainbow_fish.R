b83e2a89985d38ac7bab0e6537301195b698ff10
madisonenda <madisonenda@ucsb.edu>
2024-08-16 13:27:42 -0700

    Initial commit

---
+++ R/rainbow_fish.R
@@ -0,0 +1,23 @@
+
+#' Rainbow Fish:
+#'A function to give information on rainbow fish friends all over the ocean!
+#'
+#'
+#' @param fish_name the name of our fish friend
+#' @param city the city they live in
+#' @param fish_scales the total number of rainbow scales they have
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' rainbow_fish( "Nemo", "Coral City", 4)
+#'
+rainbow_fish <- function(fish_name, city, fish_scales) {
+
+  print( paste( fish_name,"from", city, "has", fish_scales, "rainbow scales!"))
+
+}
+
+
+
