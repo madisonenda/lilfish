a9dd4df875cc7a8e64dda8ef984f0ac29714f2db
madisonenda <madisonenda@ucsb.edu>
2024-08-23 11:06:48 -0700

    added sailboat

--- R/Basic_Functions.R
+++ R/Basic_Functions.R
@@ -32,3 +32,20 @@ math <- function( a, b,c ) {
   X= (a/b)^c
 return(X)
 }
+
+
+#' Sailboat
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sailboat <- function() {theme(title = element_text(size = 12, color = "cyan"),
+                              plot.background = element_rect(fill = "gray20"),
+                              panel.background = element_rect(fill = "lightblue"),
+                              axis.text = element_text(color = "aquamarine"),
+                              panel.grid.major = element_line(color = "blue"),
+                              panel.grid.minor = element_line(color = "yellow"),
+
+                              legend.position = "none"  )
+}
